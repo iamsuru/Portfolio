69543d02b72a170824351c1de35359de85ca4996
iamsuru <suryanshshrivastava6364@gmail.com>
2024-01-01 00:06:22 +0530

    updates on link

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,44 +1,44 @@
 const ProjectData = [
-    {   
-        title : 'NavEq',
-        type : 'WEBSITE',
-        description : ['Worked as a Team Lead and created Maps using SVG.','Worked on Front-End using HTML, CSS.','Performed key role in the development process of UI portion of web application.'],
-        link : 'https://iamsuru.github.io/NavEq-V2.0/'
+    {
+        title: 'NavEq',
+        type: 'WEBSITE',
+        description: ['Worked as a Team Lead and created Maps using SVG.', 'Worked on Front-End using HTML, CSS and React js Framework.', 'Performed key role in the development process of UI portion of web application.'],
+        link: 'https://naveq.netlify.app/'
     },
-    {   
-        title : 'DemoBank',
-        type : 'DESKTOP APPLICATION',
-        description : ['Worked on DemoBank using Java Swing, JDBC, MySQL.','Involved in high-level design, handling technical designs and complex application features.'],
-        link : 'https://github.com/iamsuru/DemoBank'
+    {
+        title: 'DemoBank',
+        type: 'DESKTOP APPLICATION',
+        description: ['Worked on DemoBank using Java Swing, JDBC, MySQL.', 'Involved in high-level design, handling technical designs and complex application features.'],
+        link: 'https://github.com/iamsuru/DemoBank'
     },
-    {   
-        title : 'SmartScan',
-        type : 'WEBAPP & ANDROID APP',
-        description : ['Contributed as a team member to a Smart India Hackathon 2022 project.','Worked on Android using Android Studio, Java & Google Firebase.'],
-        link : 'https://github.com/iamsuru/SmartScanFinal'
+    {
+        title: 'SmartScan',
+        type: 'WEBAPP & ANDROID APP',
+        description: ['Contributed as a team member to a Smart India Hackathon 2022 project.', 'Worked on Android using Android Studio, Java & Google Firebase.'],
+        link: 'https://github.com/iamsuru/SmartScanFinal'
     },
-    {   
-        title : 'Flight Booking System',
-        type : 'WEBSITE',
-        description : ['A Personal Project created using Java Enterprise Edition','Worked on Flight Booking System using JSP, Servlets & MySQL'],
-        link : 'https://github.com/iamsuru/FlightBookingSystem'
+    {
+        title: 'Flight Booking System',
+        type: 'WEBSITE',
+        description: ['A Personal Project created using Java Enterprise Edition', 'Worked on Flight Booking System using JSP, Servlets & MySQL'],
+        link: 'https://github.com/iamsuru/FlightBookingSystem'
     },
-    {   
-        title : 'SmartScan 2.0',
-        type : 'WEBAPP',
-        description : ['Enhanced Academic Major Project, an improved version of SmartScan.',`Users can upload documents using mobile's QR
-        Code Scanner without installing external Android apps.`,`Implemented with HTML, CSS, JavaScript, and Python for Front-
+    {
+        title: 'SmartScan 2.0',
+        type: 'WEBAPP',
+        description: ['Enhanced Academic Major Project, an improved version of SmartScan.', `Users can upload documents using mobile's QR
+        Code Scanner without installing external Android apps.`, `Implemented with HTML, CSS, JavaScript, and Python for Front-
         End, and Python and Google Firebase for Database API.`],
-        link : 'https://github.com/iamsuru/SmartScan_2.0'
+        link: 'https://github.com/iamsuru/SmartScan_2.0'
     },
-    {   
-        title : 'SmartScan(MERN)',
-        type : 'WEBAPP',
-        description : ['An improved version of SmartScan.',
-        'Created using HTML, CSS, JavaScript, Node.JS & Express.JS & MongoDB as a Database.','Performed key role in the development process of UI portion of web application.'],
+    {
+        title: 'SmartScan(MERN)',
+        type: 'WEBAPP',
+        description: ['An improved version of SmartScan.',
+            'Created using HTML, CSS, JavaScript, Node.JS & Express.JS & MongoDB as a Database.', 'Performed key role in the development process of UI portion of web application.'],
         link: 'https://smartscan.onrender.com/'
     }
-] 
+]
 
 
 export default ProjectData
