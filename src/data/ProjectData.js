c507c33fe2397286a33d129a2051f4181f590112
iamsuru <suryanshshrivastava6364@gmail.com>
2024-06-06 12:11:40 +0530

    Updating Projects

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -15,27 +15,27 @@ const ProjectData = [
         title: 'SmartScan',
         type: 'WEBAPP & ANDROID APP',
         description: ['Contributed as a team member to a Smart India Hackathon 2022 project.', 'Worked on Android using Android Studio, Java & Google Firebase.'],
-        link: 'https://smartscanmern.onrender.com/'
+        link: 'https://smartscan-nhmv.onrender.com/'
     },
     {
         title: 'FilmPholio',
         type: 'WEBAPP',
         description: ['FilmPholio is a web application that allows users to search for movies, view details, and create personalized movie lists.', 'These lists can be public or private, providing flexibility for users to share their favorite movies with others or keep them private.', 'The application uses the TMDB API to fetch movie details.', 'Developed using React.js, BootStrap, ChakraUI for frontend, Node.js & Express for REST API and Google FireBase for Database.'],
-        link: 'https:filmpholio.netlify.app'
+        link: 'https://filmpholio.netlify.app'
     },
     {
         title: 'ChatterBox',
         type: 'WEBAPP',
         description: ['',
             'Developed using React.js for the frontend and Node.js with Express for the backend, it seamlessly integrates MongoDB and Firebase for efficient data management.', `"ChatterBox" is a dynamic web application designed for real-time communication.`, 'Leveraging Socket.IO, users can engage in live chatting experiences.', 'This project showcases expertise in high-level design and the implementation of complex features, offering a comprehensive solution for interactive online communication.'],
-        link: 'https://github.com/iamsuru/QuickQuiz'
+        link: 'https://chatterbox-wy0q.onrender.com/'
     },
     {
         title: 'SmartScan 2.0',
         type: 'WEBAPP',
         description: ['An improved version of SmartScan.',
             'Developed using HTML, CSS, JavaScript, Node.JS & Express.JS & MongoDB as a Database.', 'Performed key role in the development process of UI portion of web application.'],
-        link: 'https://smartscanmern.onrender.com/'
+        link: 'https://smartscan-nhmv.onrender.com/'
     },
     {
         title: 'QuickQuiz',
