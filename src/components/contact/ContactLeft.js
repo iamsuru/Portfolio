00035395fa06162ee149c37c66fbd489b24f13e0
iamsuru <suryanshshrivastava6364@gmail.com>
2025-05-01 19:53:35 +0530

    feat: created contact section

---
+++ src/components/contact/ContactLeft.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { FaLinkedinIn } from "react-icons/fa";
+import { contactImg } from "../../assets/index";
+import { SiInstagram } from 'react-icons/si';
+
+const ContactLeft = () => {
+  return (
+    <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
+      <img
+        className="w-full h-64 object-cover rounded-lg mb-2"
+        src={contactImg}
+        alt="contactImg"
+      />
+      <div className="flex flex-col gap-4">
+        <h3 className="text-3xl font-bold text-white">Suryansh Shrivastava</h3>
+        <p className="text-lg font-normal text-gray-400">
+          Software Development Engineer - I
+        </p>
+        <p className="text-base text-gray-400 tracking-wide">
+          I'm always open to discussing new projects, collaboration opportunities, or anything tech-related. Feel free to reach out — let's build something impactful together!
+        </p>
+        <p className="text-base text-gray-400 flex items-center gap-2">
+          Phone: <span className="text-lightText">+91 9589661210</span>
+        </p>
+        <p className="text-base text-gray-400 flex items-center gap-2">
+          Email: <span className="text-lightText">iamsuru07@gmail.com</span>
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
+        <div className="flex gap-4">
+          <a href="https://linkedin.com/in/iamsuru/" target="_blank">
+            <span className="bannerIcon"><FaLinkedinIn /></span>
+          </a>
+          <a href="https://instagram.com/iamsuru_07/" target="_blank">
+            <span className="bannerIcon"><SiInstagram /></span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactLeft
